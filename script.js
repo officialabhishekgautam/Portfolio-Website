20b62d46ad22d837b48e186f9821bfe18c01806b
Abhishek Gautam <officialabhishekgautam@gmail.com>
2021-09-02 19:35:33 +0530

    sidebar functionality has been added, responsivity bugs fixed

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button =>
+  button.addEventListener("click", _ => {
+    document.getElementById("sidebar").classList.toggle("collapsed");
+  })
+);
