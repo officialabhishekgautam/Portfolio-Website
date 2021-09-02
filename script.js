2ab038195d1b2e3678cecd851e330edc8352832d
Abhishek Gautam <officialabhishekgautam@gmail.com>
2021-09-03 04:30:59 +0530

    form bug fixed

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ for (var i = 0; i < buttons ; i++) {
       document.getElementById("sidebar").classList.toggle("collapsed");
     });
 }
+
+
+
+function sendEmail() {
+  var name = document.getElementsByName("name");
+  var from = document.getElementsByName("email");
+  var msg = document.getElementsByName("message");
+
+  Email.send({
+      SecureToken : "460e8804-23d4-4411-bd1e-fd43af126c9b",
+      To : 'officialabhishekgautam@gmail.com',
+      From : from,
+      Subject : "Email from Portfolio Website",
+      Body : "Hi Abhishek "+name+" here,<br>"+msg,
+  }).then(
+    message => alert("Email sent")
+  );
+  }
